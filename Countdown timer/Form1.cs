99bf6dc3993b51ea37122de3027d3adac7484463
selmakhaled <salmakhaledalomar@gmail.com>
2021-01-05 17:51:15 +0200

    INITCoMiT

---
+++ Countdown timer/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Countdown_timer
+{
+    public partial class Form1 : Form
+    {
+        private int totalSeconds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.button2.Enabled = false;
+
+            for (int i = 0; i< 60; i++)
+            {
+                this.comboBox1.Items.Add(i.ToString());
+                this.comboBox2.Items.Add(i.ToString());
+            }
+            this.comboBox1.SelectedIndex = 59;
+            this.comboBox2.SelectedIndex = 59;
+        }
+
+       
+    }
+}
