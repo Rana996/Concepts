4d3e21cfd1858a6e6072c3961ecab5709c4e5ffc
Shrokayman <shrokayman99@gmail.com>
2021-01-05 18:07:00 +0300

    second commit

--- Countdown timer/Form1.cs
+++ Countdown timer/Form1.cs
@@ -31,6 +31,42 @@ namespace Countdown_timer
             this.comboBox2.SelectedIndex = 59;
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = true;
+
+            int minutes = int.Parse(this.comboBox1.SelectedItem.ToString());
+            int seconds = int.Parse(this.comboBox2.SelectedItem.ToString());
+
+            totalSeconds = (minutes * 60) + seconds;
+            this.timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Enabled = false;
+            this.button1.Enabled = true;
+
+            totalSeconds = 0;
+            this.timer1.Enabled = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (totalSeconds>0)
+            {
+                totalSeconds--;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds - (minutes*60);
+                this.label3.Text = minutes.ToString() + ":" + seconds.ToString();
+            }
+            else
+            {
+                this.timer1.Stop();
+                MessageBox.Show("Time's up");
+            }
+        }
     }
 }
